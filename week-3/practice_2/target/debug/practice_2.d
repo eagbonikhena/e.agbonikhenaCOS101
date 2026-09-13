c9e0f3c51f90cec82d4c1eadf135e13d65956bcd
Elizabeth <elizabeth.agbonikhena@pau.edu.ng>
2026-09-13 23:22:58 +0100

    My fourth commit

---
+++ week-3/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Documents\e.agbonikhenaCOS101\week-3\practice_2\target\debug\practice_2.exe: C:\Users\USER\Documents\e.agbonikhenaCOS101\week-3\practice_2\src\main.rs
---
+++ week-3/practice_8/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+   let fees = 25_000;
+   println!("fees is {} ",fees);
+   println!("fees changed is {}",fees);
+}
